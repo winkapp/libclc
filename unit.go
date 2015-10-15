9385b587e41f3a22ef67981360bb83ff14fa7363
Nathan Smith <nathansmith22@gmail.com>
2015-10-15 10:55:55 -0400

    Add unit writing logic.

--- unit.go
+++ unit.go
@@ -3,6 +3,7 @@ package libclc
 import (
   "text/template"
   "bufio"
+  "os"
 )
 
 type UnitConfig struct {
@@ -21,10 +22,43 @@ type Unit struct {
   Xfleet []string
 }
 
-func WriteUnit(unit Unit, t *template.Template) (error) {
+func WriteUnits(units *UnitConfig, t *template.Template, path string) (error) {
+  for _, service := range units.Units {
+    err := WriteUnit(service, t, path)
+    if err != nil {
+      return err
+    }
+  }
   return nil
 }
 
-func MakeUnit(t *template.Template, service *Unit, w *bufio.Writer) (error) {
+func WriteUnit(unit *Unit, t *template.Template, path string) (error) {
+  fileName := unitFileName(unit)
+  f, err := os.Create(path + "/" + fileName)
+  if err != nil {
+    return err
+  }
+  defer f.Close()
+  w := bufio.NewWriter(f)
+  err = MakeUnit(unit, t, w)
+  if err != nil {
+    return err
+  }
+  w.Flush()
+  return nil
+}
+
+func MakeUnit(service *Unit, t *template.Template, w *bufio.Writer) (error) {
   return t.Execute(w, service)
+}
+
+func unitFileName(service *Unit) string {
+  switch service.Type {
+  case "single":
+    service.Filename = (service.Name + ".service")
+  case "multi":
+    service.Filename = (service.Name + "@.service")
+  }
+
+  return service.Filename
 }
